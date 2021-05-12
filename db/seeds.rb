4f681226f9e4f41a6307ccb738d1405c14905081
Arthur Maguin <maguinarthur@gmail.com>
2021-05-12 12:36:40 +0200

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+goiko = Restaurant.create(name: 'Goiko', address: '105 rue Oberkampf', phone_number: '0629302918', category: 'french')
+
+zaatar = Restaurant.create(name: 'Zaatar', address: '99 rue Oberkampf', phone_number: '0629303123', category: 'belgian')
+
+thai_wok_house = Restaurant.create(name: 'Thai Wok House', address: '45 rue Oberkampf', phone_number: '0145671290', category: 'chinese')
+
+mamma_roma = Restaurant.create(name: 'Mamma Roma', address: '33 rue Oberkampf', phone_number: '0145784910', category: 'italian')
+
+surpriz = Restaurant.create(name: 'Surpriz', address: ' 16 rue Oberkampf', phone_number: '0147582010', category: 'belgian')
